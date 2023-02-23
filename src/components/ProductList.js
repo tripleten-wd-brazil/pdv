f0efa67342af7c36ec7b2dc247e37b9eee7d98c4
Thiago Puntar <35710202+thiagopuntar@users.noreply.github.com>
2023-02-23 19:24:18 -0300

    fix ProductList constructor

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,7 @@ import Product from "./Product.js";
 
 export default class ProductList {
   constructor(order) {
-    const products = JSON.parse(localStorage.getItem("products"));
+    const products = JSON.parse(localStorage.getItem("products")) || [];
     products.forEach((productData) => {
       const product = new Product(productData);
       product.createCard(order);
