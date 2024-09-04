f4c4cc9d16a488e2eb7c49d81f3c9ef98179c186
Thiago Puntar <thiagopuntar@gmail.com>
2024-09-04 20:41:55 -0300

    Editar perfil do vendedor
    Co-authored-by: Débora Beatriz Figueiró <deborabeatriz-f@users.noreply.github.com>
    Co-authored-by: Fernando Pereira <pereirafernando13@users.noreply.github.com>
    Co-authored-by: Vinicius Seixas <VensDev@users.noreply.github.com>
    Co-authored-by: Xande-PtBr <Xande-PtBr@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-console.log("Hello world");
+// Elemento HTML popup para remover o display none;
+let popupProfile = document.querySelector(".popup");
 
-// String
-let nomeProduto = "Coca cola"; // String
+let buttonOpenProfile = document.querySelector(".profile__button");
+buttonOpenProfile.addEventListener("click", function () {
+  popupProfile.classList.add("popup_opened");
+});
 
-// Number
-let quantidadeProduto = 2; // integer
-let precoProduto = 5.5; // float
+// Fechar popup
+// aplicar display none;
 
-let precoTotal = quantidadeProduto * precoProduto;
+let buttonCloseProfile = document.querySelector(".popup__button-close");
+buttonCloseProfile.addEventListener("click", function () {
+  popupProfile.classList.remove("popup_opened");
+});
 
-// Boolean
-let temDesconto = precoProduto >= 5;
-if (temDesconto) {
-  precoProduto = precoProduto - 1;
+// Salvar info do perfil
+//
+let formElement = document.querySelector(".popup__form");
+function handleProfileFormSubmit(evt) {
+  // Esta linha impede o navegador
+  // de enviar o formulário da forma padrão.
+  evt.preventDefault();
+  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+  // Explicaremos em mais detalhes posteriormente.
+
+  // Vamos encontrar os campos de formulário do DOM
+  let nameInput = document.querySelector(".popup__name");
+  let jobInput = document.querySelector(".popup__job");
+
+  // Pegue os valores de cada campo do valor da propriedade correspondente
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  // Selecione os elementos aos quais os valores dos campos serão inseridos
+  let nameElement = document.querySelector(".profile__title");
+  let jobElement = document.querySelector(".profile__job");
+  // Insira novos valores usando a
+  // propriedade textContent
+
+  nameElement.textContent = name;
+  jobElement.textContent = job;
 }
 
-// Object
-let produto = {
-  nome: "Coca cola",
-  quantidade: 2,
-  preco: 5.5,
-  temDesconto: true,
-};
-
-let valorTotal = produto.preco * produto.quantidade;
-
-console.log(produto);
-
-// Function
-// Declaracão
-function consolar(conteudo) {
-  console.log(conteudo);
-  return "Sucesso";
-}
-
-// Invocar / Chamar
-let meuConteudo = "Ola";
-consolar(meuConteudo);
-
-let meuResultado = consolar("Outro ola");
-console.log(meuResultado);
-
-// Outra forma de declaracao;
-let minhaFuncao = function() {
-
-}
-
-minhaFuncao()
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+formElement.addEventListener("submit", handleProfileFormSubmit);
