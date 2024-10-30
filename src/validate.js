01a0af3e5be2544b576cda56db1ab2a83a9fe0b1
Thiago Puntar <thiagopuntar@gmail.com>
2024-10-30 18:30:43 -0300

    Co-authored-by: Vinicius Seixas <VensDev@users.noreply.github.com> Co-authored-by: Débora Beatriz Figueiró <deborabeatriz-f@users.noreply.github.com>

--- src/validate.js
+++ src/validate.js
@@ -1,27 +1,51 @@
+function enableSubmitButton(form, buttonSubmit) {
+  const isFormValid = form.checkValidity();
+  if (isFormValid) {
+    buttonSubmit.disabled = false;
+  }
+}
+
+function validateInput(input, config, formElement) {
+  // verificar se o input é válido
+  const formButton = formElement.querySelector(config.submitButtonSelector);
+  const isValid = input.checkValidity();
+  const errorElement = input.nextElementSibling;
+  if (isValid) {
+    // se for válido, esconder mensagem de erro
+    errorElement.textContent = "";
+    errorElement.classList.remove(config.errorClass);
+    input.classList.remove(config.inputErrorClass);
+  } else {
+    // se for inválido, mostrar mensagem de erro
+    const errorMessage = input.validationMessage;
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(config.errorClass);
+    input.classList.add(config.inputErrorClass);
+
+    // desabilitar button do form
+    formButton.disabled = true;
+  }
+
+  // se todos os inputs forem válidos, habilitar o button do form;
+  enableSubmitButton(formElement, formButton);
+}
+
 // enableValidation
 function enableValidation(config) {
   const formElement = document.querySelector(config.formSelector);
   const inputs = formElement.querySelectorAll(config.inputSelector);
   inputs.forEach((input) => {
-    input.addEventListener("input", () => {
-      // verificar se o input é válido
-      const isValid = input.checkValidity();
-      if (isValid) {
-        // se for válido, esconder mensagem de erro
-      } else {
-        // se for inválido, mostrar mensagem de erro
-        // desabilitar button do form
-      }
-      // se todos os inputs forem válidos, habilitar o button do form;
-    });
+    input.addEventListener("input", () =>
+      validateInput(input, config, formElement)
+    );
   });
 }
 
 enableValidation({
   formSelector: ".form_edit_profile",
   inputSelector: ".form__input",
-  submitButtonSelector: ".popup__button",
+  submitButtonSelector: ".form__submit",
   inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  inputErrorClass: "form__input_error",
+  errorClass: "form__error_active",
 });
